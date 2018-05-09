452fb02d0f95d5180beeb8445157644b74896b9f
Kacper <kacper.s.kowalski@gmail.com>
2018-05-09 19:56:41 +0200

    Find new printf library for embedded systems

---
+++ FindTinyPrintf.cmake
@@ -0,0 +1,10 @@
+CMAKE_MINIMUM_REQUIRED(VERSION 3.5)
+
+IF(NOT TinyPrintf_DIR)
+	MESSAGE(FATAL_ERROR "TinyPrintf_DIR not specified")
+ENDIF()
+
+SET(TinyPrintf_SOURCES ${TinyPrintf_DIR}/printf.c)
+FILE(GLOB TinyPrintf_INCLUDE_DIR ${TinyPrintf_DIR})
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(TinyPrintf DEFAULT_MSG TinyPrintf_INCLUDE_DIRS TinyPrintf_SOURCES)
